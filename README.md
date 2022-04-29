7d12559fea154f1cff7e0b4ac7ef37f4e017a529
FOOLISH06 <1450061441@qq.com>
2022-04-29 15:03:11 +0800

    group control

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ gee 是一个 gin-like 框架，制作者[极客兔兔](https://github.com/geekt
 - day1 - 包装 net/http 接口，创建 gee 原型，实现静态路由 [Code](https://github.com/FOOLISH06/gee-learning/tree/day1)
 - day2 - 创建上下文，包装 request 和 response 信息 [Code](https://github.com/FOOLISH06/gee-learning/tree/day2)
 - day3 - 使用前缀树实现动态路由 [Code](https://github.com/FOOLISH06/gee-learning/tree/day3)
+- day4 - 分组控制，使用前缀区分，分组可嵌套 [Code](https://github.com/FOOLISH06/gee-learning/tree/day4)
--- gee/gee.go
+++ gee/gee.go
@@ -1,6 +1,7 @@
 package gee
 
 import (
+	"log"
 	"net/http"
 )
 
@@ -9,30 +10,58 @@ type HandlerFunc func(ctx *Context)
 
 // Engine implements the Handler interface.
 // it uses router to store handlers.
-type Engine struct {
-	router *router
-}
+// Engine is the sub-class of RouterGroup
+type (
+	RouterGroup struct {
+		prefix string
+		middleware []HandlerFunc // support middleware
+		engine *Engine			 // all groups share one engine instance
+	}
+
+	Engine struct {
+		*RouterGroup
+		router *router
+		groups []*RouterGroup	// store all groups
+	}
+)
 
 // New is the constructor of gee.Engine
 func New() *Engine {
 	engine := &Engine{router: newRouter()}
+	group := &RouterGroup{engine: engine}
+	engine.RouterGroup = group
+	engine.groups = []*RouterGroup{group}
 	return engine
 }
 
-// addRoute adds handler function into engine.router.handlers,
+// Group is the constructor of RouterGroup
+// all groups share an engine instance
+func (group *RouterGroup) Group(prefix string) *RouterGroup{
+	engine := group.engine
+	newGroup := &RouterGroup{
+		prefix: group.prefix + prefix,
+		engine: engine,
+	}
+	engine.groups = append(engine.groups, newGroup)
+	return newGroup
+}
+
+// addRoute adds handler function into Engine.router.handlers,
 // invoked by methods like GET(), POST()
-func (engine *Engine) addRoute(method string, pattern string, handler HandlerFunc) {
-	engine.router.addRoute(method, pattern, handler)
+func (group *RouterGroup) addRoute(method string, suffix string, handler HandlerFunc) {
+	pattern := group.prefix + suffix
+	log.Printf("Route %4s - %s\n", method, pattern)
+	group.engine.router.addRoute(method, pattern, handler)
 }
 
 // GET defines the method to add GET request handler
-func (engine *Engine) GET(pattern string, handler HandlerFunc) {
-	engine.addRoute("GET", pattern, handler)
+func (group *RouterGroup) GET(pattern string, handler HandlerFunc) {
+	group.addRoute("GET", pattern, handler)
 }
 
 // POST defines the method to add POST request handler
-func (engine *Engine) POST(pattern string, handler HandlerFunc) {
-	engine.addRoute("POST", pattern, handler)
+func (group *RouterGroup) POST(pattern string, handler HandlerFunc) {
+	group.addRoute("POST", pattern, handler)
 }
 
 // Run defines the method to start a http server
